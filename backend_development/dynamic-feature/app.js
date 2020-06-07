b721674b10e6cc86886209fe1a05c50bb37c127f
Vincent van Leeuwen <v.vanleeuwen@gillz.nl>
2020-06-07 19:48:37 +0200

    Cleanup of app.js

--- backend_development/dynamic-feature/app.js
+++ backend_development/dynamic-feature/app.js
@@ -4,50 +4,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
-const { notEqualCheck, equalCheck } = require('./public/utils/handleHelpers');
+
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-const sharedSessions = require('express-socket.io-session');
 const cookieParser = require('cookie-parser');
 
-// Require the models
-const Dog = require('./data/dogModel');
-const Message = require('./data/messageModel');
+// Import Socket IO and Mongoose
+const { initializeSocketIO } = require('./data/socket');
+const { db, dogVariables } = require('./data/mongo');
+
+// Handlebars helpers
+const { notEqualCheck, equalCheck } = require('./public/utils/handleHelpers');
 
 // Make .env file usable
 require('dotenv').config();
 
 // Set the port
 const port = 4000 || process.env.PORT;
-
-let db;
-
-// Initialize MongoDB
-const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@playdate-npesr.mongodb.net/playdatedatabase?retryWrites=true&w=majority`;
-
-// Connect to the Database
-mongoose.connect(dbUrl, {
-
-  // Prevent connection error
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-
-});
-
-db = mongoose.connection;
-
-db.on('connected', () => {
-
-  console.log(`Connected!`);
-
-});
-
-db.on('error', err => console.log(`MongoDB connection error: ${err}`));
-
 
 // Define the session
 let newSession = session({
@@ -82,6 +57,7 @@ app.engine('hbs', handlebars({
     notEqual: notEqualCheck
   }
 }))
+
 .set('view engine', 'hbs')
 
 .use(cookieParser(process.env.SESSION_SECRET))
@@ -108,108 +84,8 @@ app.engine('hbs', handlebars({
   matches
 );
 
-
-io.use(sharedSessions(newSession));
-
-// Initialize Socket.io
-io.sockets.on('connection', socket => {
-
-  // Save message to database
-  socket.on('message-to-db',(message) => {
-
-    // Push new message to the database
-    Message.create([{
-      sendFrom: socket.handshake.session.user.email,
-      sendTo: socket.handshake.session.selected.email,
-      message: message.message,
-      receiver: socket.handshake.session.user.email,
-      date: message.date
-    }]);
-
-  });
-
-  // When user clicks block (socket.emit @default.js)
-  socket.on('delete-message', (id) => {
-
-    console.log('delete', id);
-
-    Message.deleteOne({'_id': id}, err => {
-
-      if(err) throw err;
-      console.log('Succesfully deleted.');
-
-    });
-
-  });
-
-  // Unfinished Socket function.
-  socket.on('match-room', data => {
-
-    socket.join(data.email);
-
-  });
-
-  // When a dog submits a message
-  socket.on('dog-message', message => {
-
-    socket.broadcast.emit('message', message);
-
-  });
-
-  // When a dog is typing, show it to the other dog.
-  socket.on('typing', () => {
-
-    socket.broadcast.emit('typing', {username: socket.handshake.session.user.name});
-
-  });
-
-  // When user clicks on block this dog, block the dog
-  socket.on('block-user', email => {
-
-    let currentDog = Dog.getDogFromEmail(socket.handshake.session.allDogs, socket.handshake.session.user);
-
-    console.log('blocked user = ', email);
-    console.log('current user = ', socket.handshake.session.user);
-
-    socket.handshake.session.user.matches = Dog.blockMatch(email, currentDog[0].matches);
-
-    // Update matches
-    Dog.updateDog(socket.handshake.session.user)
-    .then(result => console.log("result is", result))
-    .catch(err => console.log(err));
-
-
-  });
-
-  // When a chat is opened, change req.session.selected to new dog
-  socket.on('chat-index', index => {
-
-    // Change the selected chat
-    socket.handshake.session.selected = Dog.selectedConversation(socket.handshake.session.allDogs,socket.handshake.session.user, index);
-    socket.handshake.session.save();
-
-  });
-
-});
-
-
-
-async function dogVariables(req, res, next) {
-
-  // Get dogs collection & messages collection from mongoDB
-  const allDogs = await Dog.getDogs();
-  const allMessages = await Message.getAllMessages();
-
-  // Set the session for this user
-  req.session.user = {email: req.body.email, name: req.body.name};
-  req.session.matches = Dog.dogMatches(allDogs, req.session.user);
-  req.session.allDogs = allDogs;
-  req.session.selected = Dog.selectedConversation(req.session.allDogs,req.session.user, 0);
-  req.session.messages =  Message.getMessages(allMessages, req.session.user.email, req.session.selected.email);
-
-  next();
-
-}
+// Initialize the chat function with Socket.io
+initializeSocketIO(server, newSession);
 
 
 // Listen on http://localhost:4000
---
+++ backend_development/dynamic-feature/data/mongo.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+// Require the models
+const Dog = require('./dogModel');
+const Message = require('./messageModel');
+
+require('dotenv').config();
+
+// Define db.
+let db;
+
+// Initialize MongoDB
+const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@playdate-npesr.mongodb.net/playdatedatabase?retryWrites=true&w=majority`;
+
+// Connect to the Database
+mongoose.connect(dbUrl, {
+
+  // Prevent connection error
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+});
+
+// Set db to this connection
+db = mongoose.connection;
+
+// When database is connected
+db.on('connected', () => {
+
+  console.log(`Connected!`);
+
+});
+
+// Log the error if error
+db.on('error', err => console.log(`MongoDB connection error: ${err}`));
+
+
+// Get all data from Mongo, set the current user.
+async function dogVariables(req, res, next) {
+
+  // Get dogs collection & messages collection from mongoDB
+  const allDogs = await Dog.getDogs();
+  const allMessages = await Message.getAllMessages();
+
+  // Set the session for this user
+  req.session.user = {email: req.body.email, name: req.body.name};
+  req.session.matches = Dog.dogMatches(allDogs, req.session.user);
+  req.session.allDogs = allDogs;
+  req.session.selected = Dog.selectedConversation(req.session.allDogs,req.session.user, 0);
+  req.session.messages =  Message.getMessages(allMessages, req.session.user.email, req.session.selected.email);
+
+  next();
+
+}
+
+
+
+
+
+
+module.exports = { db, dogVariables };
